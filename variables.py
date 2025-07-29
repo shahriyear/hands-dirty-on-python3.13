781e4481c0e8c4d50149069fc013ab75cd745189
Fahim Shahriyear <fahim.shahriyear@portonics.com>
2025-07-29 21:50:59 +0600

    variables

---
+++ variables.py
@@ -0,0 +1,30 @@
+# string, integer, float, boolean
+
+name = "Fahim"
+age = 21
+height = 5.9
+is_active = True
+
+print(name)
+print(f"Hello {name}")
+print(age)
+print(height)
+print(is_active)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_active))
+
+# type conversion str(), int(), float(), bool()
+print("-------------------------")
+age = str(age)
+height = int(height)
+is_active = str(is_active)
+
+
+print(type(age))
+print(type(height))
+print(type(is_active))
+
+
